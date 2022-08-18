9ed7437da08bdf36b7bb8702eaefe2cfad178e04
jdbalantar <jdbalantar@gmail.com>
2022-08-17 20:32:44 -0500

    Terminando de listar coments y precio

--- NET Core 3 - React/ProyectoCore/Dominio/Comentario.cs
+++ NET Core 3 - React/ProyectoCore/Dominio/Comentario.cs
@@ -9,7 +9,6 @@ namespace Dominio
         public int Puntaje { get; set; }
         public string ComentarioTexto { get; set; }
         public Guid CursoId { get; set; }
-
         public Curso Curso { get; set; }
 
     }
